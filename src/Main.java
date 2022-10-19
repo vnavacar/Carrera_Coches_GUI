e94f81ca8463d623dbaa4ae2b5f3535513cbcb85
vnavacar <vnavacar@myuax.com>
2022-10-19 15:22:29 +0200

    Quemar todo y volver a empezar

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main {
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println("The number is: " + number);
+        }
+    
+}
